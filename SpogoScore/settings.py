1de448a9f217f19a546d53fc8a7371213f939b1e
Varad Prabhu <varadp763@gmail.com>
2022-05-09 16:00:04 +0530

    New Deployment

--- SpogoScore/settings.py
+++ SpogoScore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','spogo-score.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','spogoscore.herokuapp.com']
 CORS_ALLOW_ALL_ORIGINS=True
 
 
